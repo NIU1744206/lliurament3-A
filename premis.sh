151a2726ef9d610c724cc0311475eb523ff21816
Adria <1744206@uab.cat>
2024-11-24 13:07:19 +0100

    sub_menu_1

--- premis.sh
+++ premis.sh
@@ -1,49 +1,97 @@
 #!/bin/bash
 
-#a)
+#Modules
+
 function menu(){
-	echo "---------------------------------------------------------"
-	echo ""
-	echo "Catàleg de guanyadors i guanyaodres d'Oscars"
-	echo ""
-	echo "---------------------------------------------------------"
-	echo "1 Llistats del catàleg."
-	echo "2 Cerca al catàleg."
-	echo "3 Com ets d’Expert?"
-	echo "4 Gestió del catàleg."
-	echo "0 Sortir."
-	read var
+	clear
+    	echo "---------------------------------------------------------"
+    	echo ""
+    	echo "Catàleg de guanyadors i guanyadores d'Oscars"
+    	echo ""
+    	echo "---------------------------------------------------------"
+    	echo "1 Llistats del catàleg."
+    	echo "2 Cerca al catàleg."
+    	echo "3 Com ets d’Expert?"
+    	echo "4 Gestió del catàleg."
+    	echo "0 Sortir."
+    	read -n 1 -s var
 }
-menu
+
+function sub_menu_1(){
+	Tasca=1
+	while [ $Tasca -ne 0 ]
+	do
+		clear
+    		echo "--------------------------------------------------"
+    		echo "         1 - Llistats del catàleg."
+    		echo "--------------------------------------------------"
+    		echo "1 Llistat d’actors i actrius per data del premi."
+    		echo "2 Llistat alfabètic de pel·lícules."
+    		echo "3 Llistat per edat d’actors i actrius."
+    		echo "0 Tornar al menú anterior."
+    		read -n 1 -s Tasca
+    		clear
+		if [ $Tasca -eq 1 ];
+		then
+			enDesenvolupament
+		fi
+    		if [ $Tasca -eq 2 ];
+    		then
+    			enDesenvolupament
+    		fi
+    		if [ $Tasca -eq 3 ];
+    		then
+    			enDesenvolupament
+    		fi
+    	done
+}
+
 function  enDesenvolupament(){
-	clear 
-	echo "En desenvolupament"
-	read var
-	clear
+    	clear 
+    	echo "En desenvolupament"
+    	read -n 1 -s desen
+    	clear
 }
+
 function opcioNoValida(){
-	clear
-	echo "ERROR"
-	sleep 2 
-	clear
-}	
-#b)
-	
+    	clear
+    	echo "ERROR"
+    	sleep 2 
+    	clear
+}
+
+#Main
+
+var=1
 while [ $var -ne 0 ]
 do
-	#c)
-	if [ $var -le 4 ] && [ $var -ge 1 ];
-	then 
-		enDesenvolupament
-		menu   
-	#d)
+	menu
+	if [ $var -le 4 ] && [ $var -ge 0 ];
+	then
+		if [ $var -eq 1 ];
+		then
+			sub_menu_1
+		fi
+		if [ $var -eq 2 ];
+		then
+			enDesenvolupament
+		fi
+		if [ $var -eq 3 ];
+		then
+			enDesenvolupament
+		fi
+		if [ $var -eq 4 ];
+		then
+			enDesenvolupament
+		fi
 	else
-		opcioNoValida
-		menu  
+		opcioNovalida	
 	fi
 done
-if [ $var -eq 0 ];
-then 
-	echo "Amor pel Setè Art"
-fi
+clear
+echo "Amor pel Sete Art"
+sleep 2
+clear
+
+
 
