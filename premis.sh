da494a1ae92685b31652a14ed7f959fab245686c
Adria <1744206@uab.cat>
2024-11-29 12:29:07 +0100

    Opcio-1.2

--- premis.sh
+++ premis.sh
@@ -17,35 +17,6 @@ function menu(){
     	read -n 1 -s var
 }
 
-function sub_menu_1(){
-	Tasca=1
-	while [ $Tasca -ne 0 ]
-	do
-		clear
-    		echo "--------------------------------------------------"
-    		echo "         1 - Llistats del catàleg."
-    		echo "--------------------------------------------------"
-    		echo "1 Llistat d’actors i actrius per data del premi."
-    		echo "2 Llistat alfabètic de pel·lícules."
-    		echo "3 Llistat per edat d’actors i actrius."
-    		echo "0 Tornar al menú anterior."
-    		read -n 1 -s Tasca
-    		clear
-		if [ $Tasca -eq 1 ];
-		then
-			enDesenvolupament
-		fi
-    		if [ $Tasca -eq 2 ];
-    		then
-    			enDesenvolupament
-    		fi
-    		if [ $Tasca -eq 3 ];
-    		then
-    			enDesenvolupament
-    		fi
-    	done
-}
-
 function  enDesenvolupament(){
     	clear 
     	echo "En desenvolupament"
@@ -70,7 +41,7 @@ do
 	then
 		if [ $var -eq 1 ];
 		then
-			sub_menu_1
+			./opcio_1 $1 $2
 		fi
 		if [ $var -eq 2 ];
 		then
